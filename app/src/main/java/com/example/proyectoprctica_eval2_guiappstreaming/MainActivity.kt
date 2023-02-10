1ccc529b9adb6799febf7f854eac0ca3a49bdf9e
daviidvh <davvel833@alu.centroafuera.es>
2023-02-10 20:25:01 +0100

    commit3

--- app/src/main/java/com/example/proyectoprctica_eval2_guiappstreaming/MainActivity.kt
+++ app/src/main/java/com/example/proyectoprctica_eval2_guiappstreaming/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.proyectoprctica_eval2_guiappstreaming
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.proyectoprctica_eval2_guiappstreaming.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(2000)
+        setTheme(R.style.Theme_ProyectoPrácticaEVAL2GUIAppStreaming)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    binding.usuario1.setOnClickListener(){
+        val intent= Intent(this, browser::class.java)
+        startActivity(intent)    }
+
     }
 }
---
+++ app/src/main/java/com/example/proyectoprctica_eval2_guiappstreaming/browser.kt
@@ -0,0 +1,11 @@
+package com.example.proyectoprctica_eval2_guiappstreaming
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class browser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_browser)
+    }
+}
